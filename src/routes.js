03ef2271f9a3eebba5dc02ecd085e50c6e2c2eb1
Dian <dianmarta136@gmail.com>
2023-04-14 11:24:44 +0700

    initial commit

---
+++ src/routes.js
@@ -0,0 +1,38 @@
+const { addNoteHandler, getAllNotesHandler, getNoteByIdHandler, editNoteByIdhandler, deleteNoteByIdHandler } = require("./handler");
+
+const routes = [
+    {
+        method: 'POST',
+        path: '/notes',
+        handler: addNoteHandler,
+        // Option:{
+        //     cors: {
+        //         origin: ['*'],
+        //     },
+        // },
+    },
+    {
+        method: 'GET',
+        path:'/notes',
+        handler: getAllNotesHandler,
+    },
+    {
+        method:'GET',
+        path: '/notes/{id}',
+        handler: getNoteByIdHandler,
+    },
+    {
+        method:'PUT',
+        path:'/notes/{id}',
+        handler: editNoteByIdhandler,
+    },
+    {
+        method:'DELETE',
+        path:'/notes/{id}',
+        handler: deleteNoteByIdHandler,
+    }
+
+];
+
+
+module.exports = routes;
